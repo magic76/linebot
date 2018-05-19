659fec42b8a9fb5ea8b58e7076caf8ac23287caf
liyanghsiao <liyanghsiao@boyu66.net>
2018-05-19 17:14:29 +0800

    init

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+var linebot = require('linebot');
+
+var bot = linebot({
+  channelId: '1581950485',
+  channelSecret: 'b65b4c323a1350d2c19b1862c1c9e030',
+  channelAccessToken: 'EJLlUxoNpvsHRwbgF+pi8QYM7EuKX+HnStVpdA99Vzw+WB3OhkUI9CPC3fIhupHCwqEFvGp1uWXQI53vQVAx/gy5JyLKQP9NI7xTKXrPpfsAggUpwjtjiNO/EEXlGEfzSigSTy2IQe/sTMchXhKr0gdB04t89/1O/w1cDnyilFU='
+});
+
+bot.on('message', function (event) {
+  event.reply(event.message.text).then(function (data) {
+    // success
+  }).catch(function (error) {
+    // error
+  });
+});
+
+bot.listen('/linewebhook', 3000);
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/linewebhook', linebotParser);
+app.listen(3000);
