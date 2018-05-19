c6e3e041cfdd70c4a8c10f578eb8b2d201a21e0c
liyanghsiao <liyanghsiao@boyu66.net>
2018-05-19 17:33:26 +0800

    update

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ bot.on('message', function (event) {
 const app = express();
 const linebotParser = bot.parser();
 app.post('/linewebhook', linebotParser);
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
