9045abf6f247e953350a893e73a74fa320dd7e3c
liyanghsiao <liyanghsiao@boyu66.net>
2018-05-20 17:55:49 +0800

    update

--- server.js
+++ server.js
@@ -13,13 +13,16 @@ bot.on('message', function (event) {
   });
 });
 
+
+bot.on('join', function (event) {
+  event.reply('join: ' + event.source.groupId);
+});
 // bot.listen('/linewebhook', 3000);
 
 const app = express();
 const linebotParser = bot.parser();
 app.post('/linewebhook', linebotParser);
 app.get('/line123', function (req, res) {
-  console.log(req);
-  bot.push('U5734ebef7dd9e53eccd4a3c05ef0eca9', 'test push');
+  bot.push('U2a4c41ed8bfd4e83f33db268b4564404', 'test push');
 });
 app.listen(process.env.PORT || 3000);
