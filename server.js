136bbb60a288da8d693cac32a3addc05d59c15bb
liyanghsiao <liyanghsiao@boyu66.net>
2018-05-23 12:12:00 +0800

    update

--- server.js
+++ server.js
@@ -2,33 +2,6 @@ const express = require('express');
 var linebot = require('linebot');
 const { parse } = require('url');
 const fetch = require('isomorphic-fetch');
-{
-  "gps_num": 9,
-  "app": "AirBox",
-  "gps_alt": 2,
-  "fmt_opt": "1",
-  "device": "臺南市龍山街",
-  "s_d2": 17,
-  "s_d0": 23,
-  "s_d1": 25,
-  "s_h0": 100,
-  "SiteName": "74DA38AF4812",
-  "gps_fix": 1,
-  "ver_app": "0.35.2",
-  "gps_lat": 22.982,
-  "s_t0": 39.12,
-  "timestamp": "2018-05-23T03:28:14Z",
-  "gps_lon": 120.222,
-  "date": "2018-05-23",
-  "tick": 1527046094,
-  "device_id": "74DA38AF4812",
-  "s_1": 100,
-  "s_0": 15,
-  "s_3": 0,
-  "s_2": 1,
-  "ver_format": "3",
-  "time": "03:28:14"
-  },
 var bot = linebot({
   channelId: '1581950485',
   channelSecret: 'b65b4c323a1350d2c19b1862c1c9e030',
@@ -72,5 +45,6 @@ app.get('/emit_message', function (req, res) {
   return ids.map((id) => {
     bot.push(id, message);
   });
+ 
 });
 app.listen(process.env.PORT || 3000);
