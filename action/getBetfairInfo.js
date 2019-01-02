d844273a692d74c003c3d5d5be147f81a0bf0dc9
liyanghsiao <liyanghsiao@boyu66.net>
2019-01-02 18:22:08 +0800

    update

--- action/getBetfairInfo.js
+++ action/getBetfairInfo.js
@@ -9,7 +9,7 @@ const getBetfairInfo = (bot, msg = '', currentId) => {
         }
     });
     results.push('https://docs.developer.betfair.com/display/1smk3cen4v3lu3yomq5qye0ni/API+Overview');
-    const message = results.filter(_ => _).join('\\n');
+    const message = results.filter(_ => _).join('\n');
     bot.push(currentId, message);
 }
 
