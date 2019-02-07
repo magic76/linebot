3a6bb9ce02b39d1fe090ce5a83be0e9284c1ccb3
liyanghsiao <liyanghsiao@boyu66.net>
2019-02-07 11:30:23 +0800

    update

--- action/getPM2_5.js
+++ action/getPM2_5.js
@@ -1,5 +1,4 @@
 const fetch = require('isomorphic-fetch');
-const fs = require('fs');
 
 const dataPush = (bot, data, msg, currentId) => {
     const feeds = data.feeds || [];
@@ -15,23 +14,12 @@ const dataPush = (bot, data, msg, currentId) => {
     });
 }
 module.exports = (bot, msg, currentId) => {
-    const fileData = require('./pm25.json');
-    console.log(fileData, fileData.time);
-    if (Date.now() - fileData.time  < 1000 * 60 * 60) {
-        dataPush(bot, fileData, msg, currentId);
-    } else {
-        fetch('https://pm25.lass-net.org/data/last-all-epa.json').then(data => data.json()).then((data) => {
+    fetch('https://pm25.lass-net.org/data/last-all-epa.json').then(data => data.json()).then((data) => {
 
-            // 將最後一筆存到file內
-            data.time = Date.now();
-            fs.writeFileSync('action/pm25.json', JSON.stringify(data));
-            console.log(JSON.stringify(require('./pm25.json')));
-            fs.writeFileSync('pm25.json', JSON.stringify(data));
-            console.log(JSON.stringify(require('./pm25.json')));
-            dataPush(bot, data, msg, currentId);
-            return ;
-        });
-    }
+        // 將最後一筆存到file內
+        dataPush(bot, data, msg, currentId);
+        return ;
+    });
 }
 
 
