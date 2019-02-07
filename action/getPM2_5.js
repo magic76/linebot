e4532515f18a4271a5669f35c7e2188bec31585a
liyanghsiao <liyanghsiao@boyu66.net>
2019-02-07 10:44:04 +0800

    pm2.5 到cache

--- action/getPM2_5.js
+++ action/getPM2_5.js
@@ -1,19 +1,37 @@
 const fetch = require('isomorphic-fetch');
-module.exports = (bot, msg, currentId) => {
-    fetch('https://pm25.lass-net.org/data/last-all-epa.json').then(data => data.json()).then((data) => {
-        const feeds = data.feeds || [];
-        const arr = [];
-        feeds.map(item => {
-          if (item.County.indexOf(msg) > -1 || item.SiteName.indexOf(msg) > -1) {
-            arr.push(item);
-          }
-        });
-        arr.map(item => {
-          bot.push(currentId, 
-            item.SiteName + '的PM2.5: ' + item['PM2_5'] + ' ' + item.Status + '\n [ '+ item.PublishTime + ' ]');
-        });
-        return ;
-        // return event.reply('end');
+const fs = require('fs');
+
+const dataPush = (bot, data, msg, currentId) => {
+    const feeds = data.feeds || [];
+    const arr = [];
+    feeds.map(item => {
+      if (item.County.indexOf(msg) > -1 || item.SiteName.indexOf(msg) > -1) {
+        arr.push(item);
+      }
+    });
+    arr.map(item => {
+      bot.push(currentId, 
+        item.SiteName + '的PM2.5: ' + item['PM2_5'] + ' ' + item.Status + '\n [ '+ item.PublishTime + ' ]');
     });
 }
+
+module.exports = (bot, msg, currentId) => {
+    const fileData = require('./pm25.json');
+    if (Date.now() - fileData.time  < 1000 * 60 * 60) {
+        dataPush(bot, fileData, msg, currentId);
+    } else {
+        fetch('https://pm25.lass-net.org/data/last-all-epa.json').then(data => data.json()).then((data) => {
+
+            // 將最後一筆存到file內
+            data.time = Date.now();
+            fs.writeFileSync('./pm25.json', JSON.stringify(data));
+
+            dataPush(bot, data, msg, currentId);
+            
+            return ;
+            // return event.reply('end');
+        });
+    }
+}
+
 
